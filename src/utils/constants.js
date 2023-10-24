458cfc39bdff34010ea9934134e2fbc1de577f74
Alexey Kostin <alexey-kostin@yandex.ru>
2023-10-24 22:38:23 +0300

    fixes: review 1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://api.kostinpract.nomoreparties.sbs/";
 export const allMoviesUrl = "https://api.nomoreparties.co";
 export const cookieExpiredTime = 10000;
 
